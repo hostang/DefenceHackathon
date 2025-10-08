af24ad1c9c81ca71fb4a395ce788b7731c641e2c
hostang <gurungpratap21@gmail.com>
2025-10-08 02:51:53 +0100

    Update main.py bigquery insert rows

--- main.py
+++ main.py
@@ -35,6 +35,10 @@ def submit_form():
         "latitude": float(form_data.get("lat", 0)),
         "longitude": float(form_data.get("lng", 0)),
     }
+    # inserting rows to big query
+    errors = bg_client.insert_rows_json(mod-hack25swi-393.hlzlandingdata.data,[row])
+    if errors:
+        return jsonify({"error": errors}),400
     return jsonify({"success": True, "inserted": row}), 201
 
 
